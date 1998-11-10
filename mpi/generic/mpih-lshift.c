95217d480da26ce81b0856cf56c5ff4c562dac36
Werner Koch <wk@gnupg.org>
1998-11-10 18:35:11 +0000

    Not yet completed changes to support PPC

--- mpi/generic/mpih-lshift.c
+++ mpi/generic/mpih-lshift.c
@@ -1,6 +1,5 @@
-/* mpihelp-shift.c  -  MPI helper functions
- *	Copyright (c) 1997 by Werner Koch (dd9jn)
- *	Copyright (C) 1994, 1996 Free Software Foundation, Inc.
+/* mpihelp-lshift.c  -	MPI helper functions
+ *	Copyright (C) 1994, 1996, 1998 Free Software Foundation, Inc.
  *
  * This file is part of G10.
  *
@@ -67,37 +66,4 @@ mpihelp_lshift( mpi_ptr_t wp, mpi_ptr_t up, mpi_size_t usize,
     return retval;
 }
 
-
-/* Shift U (pointed to by UP and USIZE limbs long) CNT bits to the right
- * and store the USIZE least significant limbs of the result at WP.
- * The bits shifted out to the right are returned.
- *
- * Argument constraints:
- * 1. 0 < CNT < BITS_PER_MP_LIMB
- * 2. If the result is to be written over the input, WP must be <= UP.
- */
-
-mpi_limb_t
-mpihelp_rshift( mpi_ptr_t wp, mpi_ptr_t up, mpi_size_t usize, unsigned cnt)
-{
-    mpi_limb_t high_limb, low_limb;
-    unsigned sh_1, sh_2;
-    mpi_size_t i;
-    mpi_limb_t retval;
-
-    sh_1 = cnt;
-    wp -= 1;
-    sh_2 = BITS_PER_MPI_LIMB - sh_1;
-    high_limb = up[0];
-    retval = high_limb << sh_2;
-    low_limb = high_limb;
-    for( i=1; i < usize; i++) {
-	high_limb = up[i];
-	wp[i] = (low_limb >> sh_1) | (high_limb << sh_2);
-	low_limb = high_limb;
-    }
-    wp[i] = low_limb >> sh_1;
-
-    return retval;
-}
 
