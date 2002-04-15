3719aa51484ed4cfa67ce71b1afd0fedde0d0c7d
Werner Koch <wk@gnupg.org>
2002-04-15 10:10:59 +0000

    * cryptmiss.c: New.

---
+++ common/cryptmiss.c
@@ -0,0 +1,39 @@
+/* cryptmiss.c - Missing functions in libgcrypt
+ *	Copyright (C) 2002 Free Software Foundation, Inc.
+ *
+ * This file is part of GnuPG.
+ *
+ * GnuPG is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * GnuPG is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA
+ */
+
+#include <config.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+#include "util.h"
+
+char *
+gcry_strdup (const char *string)
+{
+  char *p;
+
+  p = gcry_malloc (strlen (string)+1);
+  if (p)
+    strcpy (p, string);
+  return p;
+}
