4948f99eb44ede223344ba3fd1719f797b430ade
Werner Koch <wk@gnupg.org>
2002-09-20 07:40:01 +0000

    * mpicoder.c (do_get_buffer): Avoid zero length allocation. Checked that all callers behave properly when NBYTES returns 0 as the length of the allocated buffer.

--- mpi/mpicoder.c
+++ mpi/mpicoder.c
@@ -332,12 +332,15 @@ do_get_buffer( MPI a, unsigned *nbytes, int *sign, int force_secure )
     byte *p, *buffer;
     mpi_limb_t alimb;
     int i;
+    unsigned int n;
 
     if( sign )
 	*sign = a->sign;
-    *nbytes = a->nlimbs * BYTES_PER_MPI_LIMB;
-    p = buffer = force_secure || mpi_is_secure(a) ? m_alloc_secure( *nbytes)
-						  : m_alloc( *nbytes );
+    *nbytes = n = a->nlimbs * BYTES_PER_MPI_LIMB;
+    if (!n)
+      n++; /* avoid zero length allocation */
+    p = buffer = force_secure || mpi_is_secure(a) ? m_alloc_secure(n)
+						  : m_alloc(n);
 
     for(i=a->nlimbs-1; i >= 0; i-- ) {
 	alimb = a->d[i];
