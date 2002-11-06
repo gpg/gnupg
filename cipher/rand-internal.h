eb6f7ce0ad8dfa5a6643a10827e977655d2e3b99
Stefan Bellon <sbellon@sbellon.de>
2002-11-06 16:17:14 +0000

    added prototype that was missing in CVS since long ago

--- cipher/rand-internal.h
+++ cipher/rand-internal.h
@@ -31,6 +31,8 @@ int rndw32_gather_random (void (*add)(const void*, size_t, int),
                           int requester, size_t length, int level);
 int rndw32_gather_random_fast (void (*add)(const void*, size_t, int),
                                int requester );
+int rndriscos_gather_random (void (*add)(const void*, size_t, int),
+                             int requester, size_t length, int level);
 
 
 #endif /*G10_RAND_INTERNAL_H*/
