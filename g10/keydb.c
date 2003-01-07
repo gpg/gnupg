8602e35feb1e2ee33e7746809c50bd726722864d
Werner Koch <wk@gnupg.org>
2003-01-07 10:05:38 +0000

    Preparing a release candidate.

--- g10/keydb.c
+++ g10/keydb.c
@@ -1,5 +1,5 @@
 /* keydb.c - key database dispatcher
- * Copyright (C) 2001, 2002 Free Software Foundation, Inc.
+ * Copyright (C) 2001, 2002, 2003 Free Software Foundation, Inc.
  *
  * This file is part of GnuPG.
  *
