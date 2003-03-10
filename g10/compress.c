a73a76b34e259f2135848b5a42ae3325dcd9a20b
Werner Koch <wk@gnupg.org>
2003-03-10 09:59:54 +0000

    * compress.c (init_uncompress): Use a 15 bit window size so that the output of implementations which don't run for PGP 2 compatibility won't get garbled.

--- g10/compress.c
+++ g10/compress.c
@@ -116,8 +116,13 @@ init_uncompress( compress_filter_context_t *zfx, z_stream *zs )
      * PGP uses a windowsize of 13 bits. Using a negative value for
      * it forces zlib not to expect a zlib header.  This is a
      * undocumented feature Peter Gutmann told me about.
+     *    
+     * We must use 15 bits for the inflator because CryptoEx uses 15
+     * bits thus the output would get scrambled w/o error indication
+     * if we would use 13 bits.  For the uncompressing this does not
+     * matter at all.
      */
-    if( (rc = zfx->algo == 1? inflateInit2( zs, -13)
+    if( (rc = zfx->algo == 1? inflateInit2( zs, -15)
 			    : inflateInit( zs )) != Z_OK ) {
 	log_fatal("zlib problem: %s\n", zs->msg? zs->msg :
 			       rc == Z_MEM_ERROR ? "out of core" :
