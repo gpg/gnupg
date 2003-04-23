a524209170160b4047bc31f3f54856e48a806aea
Werner Koch <wk@gnupg.org>
2003-04-23 10:17:41 +0000

    Reverted recent changes in the copyright notices.  We can't use a range of years but must list each year we have prepared a release and applied a textual change to a file.  It is common that some files are not changed in a couple of years and a range would not allow to express this.

--- g10/build-packet.c
+++ g10/build-packet.c
@@ -1,5 +1,6 @@
 /* build-packet.c - assemble packets and write them
- * Copyright (C) 1998-2003 Free Software Foundation, Inc.
+ * Copyright (C) 1998, 1999, 2000, 2001, 2002,
+ *               2003 Free Software Foundation, Inc.
  *
  * This file is part of GnuPG.
  *
--- g10/keyedit.c
+++ g10/keyedit.c
@@ -1,5 +1,6 @@
 /* keyedit.c - keyedit stuff
- * Copyright (C) 1998-2003 Free Software Foundation, Inc.
+ * Copyright (C) 1998, 1999, 2000, 2001, 2002,
+ *               2003 Free Software Foundation, Inc.
  *
  * This file is part of GnuPG.
  *
