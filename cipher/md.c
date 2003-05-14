7df1d684d980d769cb5b3d24e4dc9c5c345c38b7
David Shaw <dshaw@jabberwocky.com>
2003-05-14 02:48:39 +0000

    * tiger.c: Use the U64_C macro instead of LL.
    * md.c (string_to_digest_algo): Give a warning about TIGER192 not being
    part of OpenPGP.
    
    * idea-stub.c (load_module): Catch an error if the idea module file is
    unloadable for some reason (unreadable, bad permissions, etc.)

--- cipher/md.c
+++ cipher/md.c
@@ -142,6 +142,12 @@ string_to_digest_algo( const char *string )
 	return 0;
       }
 
+#ifdef USE_TIGER
+    if(!ascii_strcasecmp("tiger192",string))
+      log_info(_("WARNING: digest `%s' is not part of OpenPGP.  "
+		 "Use at your own risk!\n"),string);
+#endif
+
     do {
 	for(r = digest_list; r; r = r->next )
 	    if( !ascii_strcasecmp( r->name, string ) )
