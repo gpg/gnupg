2c9aac608b25072c823779af56411dcf14bf0ae8
Marcus Brinkmann <mb@g10code.com>
2004-04-20 16:02:30 +0000

    2004-04-20  Marcus Brinkmann  <marcus@g10code.de>
    	* gpgconf-comp.c (gc_options_gpg_agent): Change type of
    	ignore-cache-for-signing option to GC_ARG_TYPE_NONE.

--- tools/gpgconf-comp.c
+++ tools/gpgconf-comp.c
@@ -468,7 +468,7 @@ static gc_option_t gc_options_gpg_agent[] =
      GC_ARG_TYPE_UINT32, GC_BACKEND_GPG_AGENT },
    { "ignore-cache-for-signing", GC_OPT_FLAG_RUNTIME, GC_LEVEL_BASIC,
      "gnupg", "do not use the PIN cache when signing",
-     GC_ARG_TYPE_UINT32, GC_BACKEND_GPG_AGENT },
+     GC_ARG_TYPE_NONE, GC_BACKEND_GPG_AGENT },
    { "no-grab", GC_OPT_FLAG_NONE, GC_LEVEL_EXPERT,
      "gnupg", "do not grab keyboard and mouse",
      GC_ARG_TYPE_NONE, GC_BACKEND_GPG_AGENT },
