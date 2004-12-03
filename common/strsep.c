237611cddff696bd3aeb36487ee24fd6fd4f908b
Werner Koch <wk@gnupg.org>
2004-12-03 18:38:23 +0000

    Fixed copyright comments.

--- common/strsep.c
+++ common/strsep.c
@@ -1,22 +1,27 @@
 /* strsep.c - Replacement for strsep().
- * Copyright (C) 2002 Free Software Foundation, Inc.
+ * Copyright (C) 1992, 1993, 1996, 1997, 1998, 1999,
+ *               2004 Free Software Foundation, Inc.
  *
- * This file is part of GnuPG.
+ * This file is part of the GNU C Library.
  *
- * GnuPG is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
+ * The GNU C Library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
  *
- * GnuPG is distributed in the hope that it will be useful,
+ * The GNU C Library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with the GNU C Library; if not, write to the Free
+ * Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ * 02111-1307 USA.  
  */
+
+/* Code taken from glibc-2.3.2/sysdeps/generic/strsep.c and slightly
+   modified for use with GnuPG. */
 
 #ifdef HAVE_CONFIG_H
 #include <config.h>
@@ -24,8 +29,6 @@
 #include <stdio.h>
 #include <string.h>
 
-/* Code taken from glibc-2.2.1/sysdeps/generic/strsep.c */
-#warning need to get the correct copyright years from glibc
 char *
 strsep (char **stringp, const char *delim)
 {
