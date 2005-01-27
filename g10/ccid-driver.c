963748d1b586d390e7b86bda177b396a0fc5cd6c
Werner Koch <wk@gnupg.org>
2005-01-27 10:30:28 +0000

    Updated to the version from 1.2.7.

--- g10/ccid-driver.c
+++ g10/ccid-driver.c
@@ -450,7 +450,7 @@ parse_ccid_descriptor (ccid_driver_t handle,
   if (handle->id_vendor == VENDOR_SCM
       /* FIXME: check whether it is the same
                 firmware version for all drivers.  */
-      && handle->bcd_device < 0x0513
+      && handle->bcd_device < 0x0519
       && handle->max_ifsd > 48)
     {
       DEBUGOUT ("enabling workaround for buggy SCM readers\n");
