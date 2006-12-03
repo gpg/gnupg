114b20e8be8baf23016a94d1214b64a7d6685481
David Shaw <dshaw@jabberwocky.com>
2006-12-03 06:27:49 +0000

    * keyedit.c (menu_clean): Show "already minimized" rather than "already clean" when a minimized key is minimized again.  From Dirk Traulsen.

--- g10/keyedit.c
+++ g10/keyedit.c
@@ -3249,7 +3249,12 @@ menu_clean(KBNODE keyblock,int self_only)
 	      modified=1;
 	    }
 	  else
-	    tty_printf(_("User ID \"%s\": already clean\n"),user);
+	    {
+	      tty_printf(self_only==1?
+			 "User ID \"%s\": already minimized\n":
+			 "User ID \"%s\": already clean\n",
+			 user);
+	    }
 
 	  xfree(user);
 	}
