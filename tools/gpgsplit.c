12fa2baa6266e1dd235e11dcd9697273ccc5a281
Werner Koch <wk@gnupg.org>
2009-07-21 15:44:30 +0000

    Fix i18n bug.

--- tools/gpgsplit.c
+++ tools/gpgsplit.c
@@ -85,7 +85,7 @@ my_strusage (int level)
       break;
     case 13: p = VERSION; break;
     case 17: p = PRINTABLE_OS_NAME; break;
-    case 19: p = _("Please report bugs to <@EMAIL@>.\n"); break;
+    case 19: p = "Please report bugs to <@EMAIL@>.\n"; break;
 
     case 1:
     case 40:	p =
