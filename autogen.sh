abbee36cff448bc3960ab4c20e99869ced88ded4
Werner Koch <wk@gnupg.org>
2012-11-16 10:35:33 +0100

    Try to use w64 toolchain for --build-w32.
    --

--- autogen.sh
+++ autogen.sh
@@ -62,7 +62,7 @@ if test "$1" = "--build-w32"; then
 
     # Locate the cross compiler
     crossbindir=
-    for host in i586-mingw32msvc i386-mingw32msvc mingw32; do
+    for host in i686-w64-mingw32 i586-mingw32msvc i386-mingw32msvc mingw32; do
         if ${host}-gcc --version >/dev/null 2>&1 ; then
             crossbindir=/usr/${host}/bin
             conf_CC="CC=${host}-gcc"
