76b1940ad6f5f058c4a6bec35902da9f6d4e27b0
Werner Koch <wk@gnupg.org>
2014-03-07 09:46:44 +0100

    w32: Define WINVER only if needed.
    * common/sysutils.c (WINVER): Define only if less that 5.0.

--- common/sysutils.c
+++ common/sysutils.c
@@ -54,7 +54,9 @@
 # include <sys/resource.h>
 #endif
 #ifdef HAVE_W32_SYSTEM
-# define WINVER 0x0500  /* Required for AllowSetForegroundWindow.  */
+# if WINVER < 0x0500
+#   define WINVER 0x0500  /* Required for AllowSetForegroundWindow.  */
+# endif
 # include <windows.h>
 #endif
 #ifdef HAVE_NPTH
