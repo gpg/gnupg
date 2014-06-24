5214d8f02bf65fb0a4af15ff80cf1369ccd4c167
Werner Koch <wk@gnupg.org>
2014-06-24 09:53:46 +0200

    gpg: Make show-uid-validity the default.

--- g10/gpg.c
+++ g10/gpg.c
@@ -2085,9 +2085,11 @@ main (int argc, char **argv)
     opt.keyserver_options.export_options = EXPORT_ATTRIBUTES;
     opt.keyserver_options.options = (KEYSERVER_HONOR_KEYSERVER_URL
                                      | KEYSERVER_HONOR_PKA_RECORD );
-    opt.verify_options = (VERIFY_SHOW_POLICY_URLS
+    opt.verify_options = (LIST_SHOW_UID_VALIDITY
+                          | VERIFY_SHOW_POLICY_URLS
                           | VERIFY_SHOW_STD_NOTATIONS
                           | VERIFY_SHOW_KEYSERVER_URLS);
+    opt.list_options   = LIST_SHOW_UID_VALIDITY;
 #ifdef NO_TRUST_MODELS
     opt.trust_model = TM_ALWAYS;
 #else
