bcf44e2d153792e20036a26126ad77cef79a0304
Werner Koch <wk@gnupg.org>
2015-02-27 09:33:58 +0100

    Release 1.4.19.

--- g10/gpg.c
+++ g10/gpg.c
@@ -1,7 +1,6 @@
 /* gpg.c - The GnuPG utility (main for gpg)
- * Copyright (C) 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006,
- *               2007, 2008, 2009, 2010, 2012 Free Software Foundation, Inc.
- * Copyright (C) 1997, 1998, 2013 Werner Koch
+ * Copyright (C) 1998-2015 Free Software Foundation, Inc.
+ * Copyright (C) 1997-2015 Werner Koch
  *
  * This file is part of GnuPG.
  *
