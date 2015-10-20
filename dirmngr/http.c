a79045e38d239a7f6e787cf7c1132772c737cc0e
Neal H. Walfield <neal@g10code.com>
2015-10-20 20:53:40 +0200

    dirmngr: Allow building with libassuan < 2.3.
    * dirmngr/http.c (send_request): Use newer assuan function only if
    available.
    
    --
    Signed-off-by: Neal H. Walfield <neal@g10code.com>
    Regression-due-to: 4e42ad30

--- dirmngr/http.c
+++ dirmngr/http.c
@@ -1475,7 +1475,9 @@ send_request (http_t hd, const char *httphost, const char *auth,
     {
       int mode;
 
+#if ASSUAN_VERSION_NUMBER >= 0x020300 /* >= 2.3.0 */
       if (assuan_sock_get_flag (ASSUAN_INVALID_FD, "tor-mode", &mode) || !mode)
+#endif
         {
           log_error ("TOR support is not available\n");
           return gpg_err_make (default_errsource, GPG_ERR_NOT_IMPLEMENTED);
