ffe60eb3d2b8f7d6c506804ce4645d695c91f237
Werner Koch <wk@gnupg.org>
2015-10-21 17:38:33 +0200

    common: Add more replacement error codes.
    * common/util.h (GPG_ERR_SERVER_FAILED): New.
    (GPG_ERR_NO_KEY): New.
    (GPG_ERR_NO_NAME): New.
    
    Signed-off-by: Werner Koch <wk@gnupg.org>

--- common/util.h
+++ common/util.h
@@ -126,6 +126,9 @@
 # define GPG_ERR_LDAP_PROX_AUTH_DENIED      891
 #endif /*GPG_ERROR_VERSION_NUMBER < 0x011300*/
 #if GPG_ERROR_VERSION_NUMBER < 0x011500  /* 1.21 */
+# define GPG_ERR_SERVER_FAILED              219
+# define GPG_ERR_NO_KEY                     220
+# define GPG_ERR_NO_NAME                    221
 # define GPG_ERR_TRUE                       255
 # define GPG_ERR_FALSE                      256
 #endif
