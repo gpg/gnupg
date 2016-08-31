76304a971fe507ea659b952932ea899463ab7166
Werner Koch <wk@gnupg.org>
2016-08-31 08:37:51 +0200

    gpg: Fix regression in gpgv's printing of the keyid.
    * g10/keyid.c (keystr): Take care of KF_NONE != KF_DEFAULT.
    --
    
    Debian-bug-id: 836144
    Signed-off-by: Werner Koch <wk@gnupg.org>

--- g10/keyid.c
+++ g10/keyid.c
@@ -403,6 +403,8 @@ keystr (u32 *keyid)
   static char keyid_str[KEYID_STR_SIZE];
   int format = opt.keyid_format;
 
+  if (format == KF_DEFAULT)
+    format = KF_NONE;
   if (format == KF_NONE)
     format = KF_LONG;
 
