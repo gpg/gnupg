1f90d3c71e3e48d0c4b7722a607464876bb71794
Neal H. Walfield <neal@g10code.com>
2016-09-07 08:55:17 +0200

    g10: Add missing sqrtu32.h and sqrtu32.c.
    --
    Signed-off-by: Neal H. Walfield <neal@g10code.com>
    Fixes-commit: 875ac92.

---
+++ g10/sqrtu32.h
@@ -0,0 +1,14 @@
+/* sqrtu32.h - Return the very approximate sqrt of an unsigned integer.
+ *
+ * This file by g10 Code GmbH
+ *
+ * To the extent possible under law, the person who associated CC0 with
+ * g10 Code GmbH has waived all copyright and related or neighboring rights
+ * to this file.
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work.  If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+
+/* Return the very approximate square root of an unsigned integer.  */
+float sqrtu32 (unsigned int i);
--- g10/tofu.c
+++ g10/tofu.c
@@ -41,9 +41,7 @@
 #include "mkdir_p.h"
 #include "gpgsql.h"
 #include "status.h"
-/* #include "sqrtu32.h" */
-#define sqrtu32(a)  (a)  /* TEMP hack due to missing commit.  */
-
+#include "sqrtu32.h"
 
 #include "tofu.h"
 
