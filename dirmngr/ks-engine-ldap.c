9e6f8a55ed04f876635792125858ee76a948802a
Justus Winter <justus@g10code.com>
2016-09-29 14:17:24 +0200

    dirmngr: Fix STARTTLS on LDAP connections.
    * dirmngr/ks-engine-ldap.c (my_ldap_connect): Fix unfortunate typo.
    --
    Courtesy of pkgsrc contributor fhajny.
    
    Signed-off-by: Justus Winter <justus@g10code.com>

--- dirmngr/ks-engine-ldap.c
+++ dirmngr/ks-engine-ldap.c
@@ -513,7 +513,7 @@ my_ldap_connect (parsed_uri_t uri, LDAP **ldap_connp,
 #endif /* LDAP_OPT_X_TLS_CACERTFILE && HAVE_LDAP_SET_OPTION */
 #endif
 
-#ifndef HAVE_LDAP_START_TLS_S
+#ifdef HAVE_LDAP_START_TLS_S
   if (uri->use_tls)
     {
       /* XXX: We need an option to determine whether to abort if the
