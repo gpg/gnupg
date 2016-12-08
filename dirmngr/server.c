2e734a3ce159de8fb60df2bd5d454f98ca710717
Justus Winter <justus@g10code.com>
2016-12-08 12:51:52 +0100

    dirmngr,build: Remove support for ADNS.
    * autogen.rc: Remove '--with-adns' argument.
    * configure.ac: Remove check for ADNS.
    * dirmngr/dns-stuff.c: Remove all code that uses ADNS.
    * dirmngr/server.c (cmd_getinfo): Update status line.
    * doc/dirmngr.texi: Do not mention ADNS.
    --
    
    We used ADNS to support queries over Tor.  However, our patch to ADNS
    that adds Tor support was never accepted upstream.  Furthermore, there
    are other shortcomings that let us to consider alternatives.
    
    Signed-off-by: Justus Winter <justus@g10code.com>

--- dirmngr/server.c
+++ dirmngr/server.c
@@ -2314,13 +2314,7 @@ cmd_getinfo (assuan_context_t ctx, char *line)
           (ctx, "- Forced use of System resolver (w/o Tor support)");
       else
         {
-#if USE_ADNS && HAVE_ADNS_IF_TORMODE
-          assuan_set_okay_line (ctx, "- ADNS with Tor support");
-#elif USE_ADNS
-          assuan_set_okay_line (ctx, "- ADNS w/o Tor support");
-#else
           assuan_set_okay_line (ctx, "- System resolver (w/o Tor support)");
-#endif
         }
       err = 0;
     }
