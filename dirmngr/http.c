3d670fa973a03ea88b5f9459b3222a951136dd7a
Justus Winter <justus@g10code.com>
2017-07-18 13:39:29 +0200

    dirmngr: Fix memory leak.
    * dirmngr/http.c (http_release_parsed_uri): Free 'params'.
    
    Signed-off-by: Justus Winter <justus@g10code.com>

--- dirmngr/http.c
+++ dirmngr/http.c
@@ -1255,6 +1255,11 @@ http_release_parsed_uri (parsed_uri_t uri)
     {
       uri_tuple_t r, r2;
 
+      for (r = uri->params; r; r = r2)
+	{
+	  r2 = r->next;
+	  xfree (r);
+	}
       for (r = uri->query; r; r = r2)
 	{
 	  r2 = r->next;
