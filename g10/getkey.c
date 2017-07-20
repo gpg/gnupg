9998b162b47931fb8a8ed961d53418d505358888
Marcus Brinkmann <marcus.brinkmann@ruhr-uni-bochum.de>
2017-07-20 17:41:49 +0200

    g10: Return proper error when gpg-agent fails to start during probe.
    * g10/getkey.c (lookup): Return immediately on any other error than
    GPG_ERR_NO_SECKEY from agent_probe_any_secret_key.
    
    Signed-off-by: Marcus Brinkmann <mb@g10code.com>
    GnuPG-bug-id: 2204

--- g10/getkey.c
+++ g10/getkey.c
@@ -3741,8 +3741,14 @@ lookup (ctrl_t ctrl, getkey_ctx_t ctx, int want_secret,
 	  goto skip;
 	}
 
-      if (want_secret && agent_probe_any_secret_key (NULL, keyblock))
-        goto skip; /* No secret key available.  */
+      if (want_secret)
+	{
+	  rc = agent_probe_any_secret_key (NULL, keyblock);
+	  if (gpg_err_code(rc) == GPG_ERR_NO_SECKEY)
+	    goto skip; /* No secret key available.  */
+	  if (rc)
+	    goto found; /* Unexpected error.  */
+	}
 
       /* Warning: node flag bits 0 and 1 should be preserved by
        * merge_selfsigs.  */
