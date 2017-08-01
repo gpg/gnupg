fde9a8cc6c849fb21f3e6782dbd5c6bc863357eb
NIIBE Yutaka <gniibe@fsij.org>
2017-08-01 11:44:52 +0900

    Simple typo fix.
    * tools/rfc822parse.c: Fix.
    
    Signed-off-by: NIIBE Yutaka <gniibe@fsij.org>

--- tools/rfc822parse.c
+++ tools/rfc822parse.c
@@ -14,7 +14,7 @@
  *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program; if not, see <https://www.gnu.org/licenses/>.
-g */
+ */
 
 
 /* According to RFC822 binary zeroes are allowed at many places. We do
