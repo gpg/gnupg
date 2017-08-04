9bb13a0e819334681caca38c9074bd7bfc04e45e
Werner Koch <wk@gnupg.org>
2017-08-04 22:19:37 +0200

    gpg: Make --no-auto-key-retrieve gpgconf-igurable.
    * g10/gpg.c (gpgconf_list): Print no-auto-key-retrieve instead of
    auto-key-retrieve.
    * tools/gpgconf-comp.c (gc_options_gpg): Replace auto-key-retrieve by
    no-auto-key-retrieve and chnage level from invisible to advanced.
    
    Signed-off-by: Werner Koch <wk@gnupg.org>

--- tools/gpgconf-comp.c
+++ tools/gpgconf-comp.c
@@ -755,7 +755,7 @@ static gc_option_t gc_options_gpg[] =
    { "auto-key-locate", GC_OPT_FLAG_NONE, GC_LEVEL_ADVANCED,
      "gnupg", N_("|MECHANISMS|use MECHANISMS to locate keys by mail address"),
      GC_ARG_TYPE_STRING, GC_BACKEND_GPG },
-   { "auto-key-retrieve", GC_OPT_FLAG_NONE, GC_LEVEL_INVISIBLE,
+   { "no-auto-key-retrieve", GC_OPT_FLAG_NONE, GC_LEVEL_ADVANCED,
      NULL, NULL, GC_ARG_TYPE_NONE, GC_BACKEND_GPG },
 
 
