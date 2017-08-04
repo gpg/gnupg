b54d75fb1dcfa2cebb3a2497b81ffb49acac2056
Werner Koch <wk@gnupg.org>
2017-08-04 17:09:17 +0200

    gpg: Avoid double fingerprint printing with import-show.
    * g10/import.c (import_one) <IMPORT_SHOW>: Take care of fingerprint
    options.
    
    Signed-off-by: Werner Koch <wk@gnupg.org>

--- g10/import.c
+++ g10/import.c
@@ -1778,7 +1778,8 @@ import_one (ctrl_t ctrl,
       merge_keys_done = 1;
       /* Note that we do not want to show the validity because the key
        * has not yet imported.  */
-      list_keyblock_direct (ctrl, keyblock, 0, 0, 1, 1);
+      list_keyblock_direct (ctrl, keyblock, 0, 0,
+                            opt.fingerprint || opt.with_fingerprint, 1);
       es_fflush (es_stdout);
     }
 
