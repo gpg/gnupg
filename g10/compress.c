0f8fd95ab32a6d29dac79e19f0850037c7d0c16f
NIIBE Yutaka <gniibe@fsij.org>
2018-04-13 10:09:02 +0900

    g10: Push compress filter only if compressed.
    * g10/compress.c (handle_compressed): Fix memory leak.
    
    --
    
    (backport from STABLE-BRANCH-2-2 commit:
    c31abf84659dbda5503dd9f3aa3449520bcd1b84)
    
    All other calls of push_compress_filter checks ALGO,
    so, do it here, too.
    
    GnuPG-bug-id: 3898
    Signed-off-by: NIIBE Yutaka <gniibe@fsij.org>

--- g10/compress.c
+++ g10/compress.c
@@ -313,15 +313,18 @@ int
 handle_compressed( void *procctx, PKT_compressed *cd,
 		   int (*callback)(IOBUF, void *), void *passthru )
 {
-    compress_filter_context_t *cfx;
     int rc;
 
     if(check_compress_algo(cd->algorithm))
       return G10ERR_COMPR_ALGO;
-    cfx = xmalloc_clear (sizeof *cfx);
-    cfx->release = release_context;
-    cfx->algo = cd->algorithm;
-    push_compress_filter(cd->buf,cfx,cd->algorithm);
+    if(cd->algorithm) {
+        compress_filter_context_t *cfx;
+
+        cfx = xmalloc_clear (sizeof *cfx);
+        cfx->release = release_context;
+        cfx->algo = cd->algorithm;
+        push_compress_filter(cd->buf,cfx,cd->algorithm);
+    }
     if( callback )
 	rc = callback(cd->buf, passthru );
     else
