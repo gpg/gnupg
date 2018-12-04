9b538451682c704b4036c0ecdb7e6b0ef8570016
Werner Koch <wk@gnupg.org>
2018-12-04 12:32:28 +0100

    gpg: New list-option "show-only-fpr-mbox".
    * g10/gpg.c (parse_list_options): Add option "show-only-fpr-mbox".
    * g10/options.h (LIST_SHOW_ONLY_FPR_MBOX): New.
    * g10/keylist.c (list_keyblock_simple): New.
    (list_keyblock): Call it.
    (list_all): Do not print the keyring name in LIST_SHOW_ONLY_FPR_MBOX
    mode.
    --
    
    Signed-off-by: Werner Koch <wk@gnupg.org>
    (cherry picked from commit 0e8bf204791ebfd0c9a8e4b49fbadf998ec62e49)
    
    * g10/keylist.c (list_keyblock_simple): Remove optional arg from
    mailbox_from_userid

--- g10/options.h
+++ g10/options.h
@@ -378,6 +378,7 @@ EXTERN_UNLESS_MAIN_MODULE int memory_stat_debug_mode;
 #define LIST_SHOW_SIG_EXPIRE             (1<<9)
 #define LIST_SHOW_SIG_SUBPACKETS         (1<<10)
 #define LIST_SHOW_USAGE                  (1<<11)
+#define LIST_SHOW_ONLY_FPR_MBOX          (1<<12)
 
 #define VERIFY_SHOW_PHOTOS               (1<<0)
 #define VERIFY_SHOW_POLICY_URLS          (1<<1)
