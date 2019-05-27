cc6069ac6ecd57dcbb808f28d54fd9f89dc55014
Werner Koch <wk@gnupg.org>
2019-05-27 10:40:38 +0200

    gpg: Allow deletion of subkeys with --delete-[secret-]key.
    * common/userids.c (classify_user_id): Do not set the EXACT flag in
    the default case.
    * g10/export.c (exact_subkey_match_p): Make static,
    * g10/delkey.c (do_delete_key): Implement subkey only deleting.
    --
    
    GnuPG-bug-id: 4457

--- g10/main.h
+++ g10/main.h
@@ -414,6 +414,8 @@ void export_print_stats (export_stats_t stats);
 int parse_export_options(char *str,unsigned int *options,int noisy);
 gpg_error_t parse_and_set_export_filter (const char *string);
 
+int exact_subkey_match_p (KEYDB_SEARCH_DESC *desc, kbnode_t node);
+
 int export_pubkeys (ctrl_t ctrl, strlist_t users, unsigned int options,
                     export_stats_t stats);
 int export_seckeys (ctrl_t ctrl, strlist_t users, unsigned int options,
