133248b297a1d72897f280d8bd21081cd6ebd66c
NIIBE Yutaka <gniibe@fsij.org>
2020-02-28 15:56:53 +0900

    scd: Fix pinpad handling when KDF enabled.
    * scd/app-openpgp.c (do_getattr): Send the KDF DO information.
    
    --
    
    Fixes-commit: 95c7498b76231d3297541172d878f6a26702539b
    Signed-off-by: NIIBE Yutaka <gniibe@fsij.org>
    (cherry picked from commit 11da441016222337284c519ff56aca34e3042373)

--- scd/app-openpgp.c
+++ scd/app-openpgp.c
@@ -1124,6 +1124,8 @@ do_getattr (app_t app, ctrl_t ctrl, const char *name)
             app->app_local->pinpad.disabled = 1;
           else
             app->app_local->pinpad.disabled = 0;
+
+          send_status_info (ctrl, table[idx].name, value, valuelen, NULL, 0);
         }
       else
         send_status_info (ctrl, table[idx].name, value, valuelen, NULL, 0);
