270c49b8c6eaf99df7b417f9d0e45eba0acfb423
NIIBE Yutaka <gniibe@fsij.org>
2020-09-07 14:24:48 +0900

    scd: Fix handling 0x00FA to support OpenPGP card 3.4.
    * scd/app-openpgp.c (data_objects): It may be longer.
    
    Fixes-commit: 90d0072165cc5c6888f14462392a211de0c4b232
    Signed-off-by: NIIBE Yutaka <gniibe@fsij.org>

--- scd/app-openpgp.c
+++ scd/app-openpgp.c
@@ -121,7 +121,7 @@ static struct {
   { 0x00D7, 0, 0x6E, 1, 0, 0, 0, 0, "UIF for Decryption"},
   { 0x00D8, 0, 0x6E, 1, 0, 0, 0, 0, "UIF for Authentication"},
   { 0x00F9, 0,    0, 1, 0, 0, 0, 0, "KDF data object"},
-  { 0x00FA, 0,    0, 1, 0, 0, 0, 0, "Algorithm Information"},
+  { 0x00FA, 0,    0, 1, 0, 0, 0, 2, "Algorithm Information"},
   { 0 }
 };
 
