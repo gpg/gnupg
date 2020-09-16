563db31467b2e3f588db4b45bade82c27f039b83
Gavin L. Rebeiro <glr@e2eops.io>
2020-09-16 11:11:43 +0000

    doc: Fix typos
    --
    GnuPG-bug-id: 5071
    
    Also fixed one in keyformat.txt [wk].
    
    (cherry picked from commit 572bcacc287d24d0a2cc56442f9fb6a9ac49e12d)

--- scd/app-openpgp.c
+++ scd/app-openpgp.c
@@ -3340,7 +3340,7 @@ build_ecc_privkey_template (app_t app, int keyno,
 }
 
 
-/* Helper for do_writekley to change the size of a key.  Not ethat
+/* Helper for do_writekey to change the size of a key.  Note that
    this deletes the entire key without asking.  */
 static gpg_error_t
 change_keyattr (app_t app, int keyno, const unsigned char *buf, size_t buflen,
