e742c17091d1bfdf55b2cc6e83e5e1aec7bfdafb
git <systems@200013.net>
2022-05-25 08:30:49 +0200

    added shell script to help publishing the location of your key in a dns record (pka) it is an alternative to make-dns-cert.c which I tried to use with a limited success.

---
+++ tools/create-dns-pka.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+## Unsure why this offset is needed and present in make-dns-cert
+TOTAL_LENGTH=6
+
+function usage {
+        echo "Usage: "$0" -k [fingerprint of a key in your keyring] -d [dns record you want to create] -u [url where to fetch the key from]"
+        echo "Example: create-dns-pka.sh -f 0BE0E990E02DF48CE0A1F199530843F3420AC0D3 -d bobdomain.com -u https://bobdomain.com/gpg/bobkey.pub"
+	echo "Will return : mzbi7kpidyqxg7gzxmpyf7b8n52fmm1n._pka   TYPE37  \# 67 0006 0000 00 14 0CE0E980E01DF48CE0B1F199540843F3420AD0D3 68747470733a2f2f3230303031332e6e65742f6770672f6c756340323030
+4031332e6e65742e707562 "
+        echo '   -f   fingerprint of a gpg key already imported in your local keyring.'
+	echo '   -d   the domain name where you want to publish this record'
+	echo '   -u   the url that points to your public key'
+        exit 1
+}
+
+while getopts "f:d:u:" opt; do
+  case $opt in
+    f)
+      FINGERPRINT=${OPTARG}
+      ;;
+    d)
+      RR=${OPTARG}
+      ;;
+    u)
+      URL=${OPTARG}
+      ;;
+    *)
+      usage
+      ;;
+  esac
+done
+
+if [ -z "$FINGERPRINT" ] || [[ "$FINGERPRINT" == "?" ]] || [ -z "$RR" ] || [[ "$RR" == "?" ]] || [ -z "$URL" ] || [[ "$URL" == "?" ]]
+  then 
+    usage
+else
+  Z_BASE32_SHA1=$(gpg --list-keys --with-wkd-hash ${FINGERPRINT} | grep -E -v "^pub|^uid|^sub" | grep "@" | sed s/\ //g | awk -F'@' '{print $1}')
+fi
+
+LENGTH_FINGERPRINT=${#FINGERPRINT}
+HEX_LENGTH_FINGERPRINT=$(printf '%x' $LENGTH_FINGERPRINT)
+HEX_LENGTH_FINGERPRINT_BY_TWO=$((HEX_LENGTH_FINGERPRINT/2))
+TOTAL_LENGTH=$((${TOTAL_LENGTH}+$((${#FINGERPRINT}/2))+${#URL}))
+HEX_URL=$(echo -n ${URL} | xxd -p | awk '{ print toupper($0) }')
+
+#printf "${Z_BASE32_SHA1}._pka\tTYPE37\t\# ${TOTAL_LENGTH} 0006 0000 00 ${HEX_LENGTH_FINGERPRINT_BY_TWO} ${FINGERPRINT} ${HEX_URL}\n" 
+printf "${Z_BASE32_SHA1}._pka\tTYPE37\t\# ${TOTAL_LENGTH} 0006 0000 00 ${HEX_LENGTH_FINGERPRINT_BY_TWO} ${FINGERPRINT} $(echo ${HEX_URL} | sed s/\ //g)\n"
