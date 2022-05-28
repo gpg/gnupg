17709e117e3a4cc15d2ae879cc7369bf8794e32b
luciano <70525822+ba9f11ecc3497d9993b933fdc2bd61e5@users.noreply.github.com>
2022-05-28 23:07:10 +0200

    Update create-dns-pka.sh

--- tools/create-dns-pka.sh
+++ tools/create-dns-pka.sh
@@ -4,7 +4,7 @@
 TOTAL_LENGTH=6
 
 function usage {
-        echo "Usage: "$0" -k [fingerprint of a key in your keyring] -d [dns record you want to create] -u [url where to fetch the key from]"
+        echo "Usage: "$0" -f [fingerprint of a key in your keyring] -d [dns record you want to create] -u [url where to fetch the key from]"
         echo "Example: create-dns-pka.sh -f 0BE0E990E02DF48CE0A1F199530843F3420AC0D3 -d bobdomain.com -u https://bobdomain.com/gpg/bobkey.pub"
 	echo "Will return : mzbi7kpidyqxg7gzxmpyf7b8n52fmm1n._pka   TYPE37  \# 67 0006 0000 00 14 0CE0E980E01DF48CE0B1F199540843F3420AD0D3 68747470733a2f2f3230303031332e6e65742f6770672f6c756340323030
 4031332e6e65742e707562 "
@@ -44,5 +44,4 @@ HEX_LENGTH_FINGERPRINT_BY_TWO=$((HEX_LENGTH_FINGERPRINT/2))
 TOTAL_LENGTH=$((${TOTAL_LENGTH}+$((${#FINGERPRINT}/2))+${#URL}))
 HEX_URL=$(echo -n ${URL} | xxd -p | awk '{ print toupper($0) }')
 
-#printf "${Z_BASE32_SHA1}._pka\tTYPE37\t\# ${TOTAL_LENGTH} 0006 0000 00 ${HEX_LENGTH_FINGERPRINT_BY_TWO} ${FINGERPRINT} ${HEX_URL}\n" 
 printf "${Z_BASE32_SHA1}._pka\tTYPE37\t\# ${TOTAL_LENGTH} 0006 0000 00 ${HEX_LENGTH_FINGERPRINT_BY_TWO} ${FINGERPRINT} $(echo ${HEX_URL} | sed s/\ //g)\n"
