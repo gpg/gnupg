e3e7806805197d9ad460b27192d9371e34d77b8d
luciano <70525822+ba9f11ecc3497d9993b933fdc2bd61e5@users.noreply.github.com>
2022-05-28 23:18:31 +0200

    Update create-dns-pka.sh

--- tools/create-dns-pka.sh
+++ tools/create-dns-pka.sh
@@ -4,11 +4,11 @@
 TOTAL_LENGTH=6
 
 function usage {
-        echo "Usage: "$0" -f [fingerprint of a key in your keyring] -u [url where to fetch the key from]"
-        echo "Example: create-dns-pka.sh -f 0BE0E990E02DF48CE0A1F199530843F3420AC0D3 -u https://bobdomain.com/gpg/bobkey.pub"
+	echo "Usage: "$0" -f [fingerprint of a key in your keyring] -u [url where to fetch the key from]"
+	echo "Example: create-dns-pka.sh -f 0BE0E990E02DF48CE0A1F199530843F3420AC0D3 -u https://bobdomain.com/gpg/bobkey.pub"
 	echo "Will return : mzbi7kpidyqxg7gzxmpyf7b8n52fmm1n._pka   TYPE37  \# 67 0006 0000 00 14 0CE0E980E01DF48CE0B1F199540843F3420AD0D3 68747470733a2f2f3230303031332e6e65742f6770672f6c756340323030
 4031332e6e65742e707562 "
-        echo '   -f   fingerprint of a gpg key already imported in your local keyring.'
+	echo '   -f   fingerprint of a gpg key already imported in your local keyring.'
 	echo '   -u   the url that points to your public key'
         exit 1
 }
