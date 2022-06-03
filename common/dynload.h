59c481bb86464953e526298bfe73c3466387c46d
Werner Koch <wk@gnupg.org>
2022-06-03 11:11:28 +0200

    common,w32: Use LoadLibraryEx to fix a warning in mingw.
    * common/dynload.h (dlopen): Use LoadLibraryEx and remove Windows CE
    support.
    --
    
    Mingw's libloadapi.h has no prototype for LoadLibrary.  Thus we use
    LoadLibraryEx which is available Since Windows XP.

--- common/dynload.h
+++ common/dynload.h
@@ -45,14 +45,9 @@ static inline void *
 dlopen (const char *name, int flag)
 {
   void *hd;
-#ifdef HAVE_W32CE_SYSTEM
-  wchar_t *wname = utf8_to_wchar (name);
-  hd = wname? LoadLibrary (wname) : NULL;
-  xfree (wname);
-#else
-  hd = LoadLibrary (name);
-#endif
   (void)flag;
+
+  hd = LoadLibraryEx (name, NULL, 0);
   return hd;
 }
 
@@ -61,13 +56,7 @@ dlsym (void *hd, const char *sym)
 {
   if (hd && sym)
     {
-#ifdef HAVE_W32CE_SYSTEM
-      wchar_t *wsym = utf8_to_wchar (sym);
-      void *fnc = wsym? GetProcAddress (hd, wsym) : NULL;
-      xfree (wsym);
-#else
       void *fnc = GetProcAddress (hd, sym);
-#endif
       if (!fnc)
         return NULL;
       return fnc;
