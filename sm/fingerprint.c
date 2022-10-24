6bd0dd762c0d6b6860894152cb51e23b0a55bfba
Werner Koch <wk@gnupg.org>
2022-10-24 17:38:49 +0200

    gpgsm: Allow ECC encryption keys with just keyAgreement specified.
    * sm/certlist.c (cert_usage_p): Allow keyAgreement for ECC.
    * sm/fingerprint.c (gpgsm_is_ecc_key): New.
    --
    
    For ECC encryption keys keyAgreement is the keyUsage we want.

--- sm/fingerprint.c
+++ sm/fingerprint.c
@@ -306,6 +306,14 @@ gpgsm_get_key_algo_info (ksba_cert_t cert, unsigned int *nbits)
 }
 
 
+/* Return true if CERT is an ECC key.  */
+int
+gpgsm_is_ecc_key (ksba_cert_t cert)
+{
+  return GCRY_PK_ECC == gpgsm_get_key_algo_info2 (cert, NULL, NULL);
+}
+
+
 /* This is a wrapper around pubkey_algo_string which takes a KSBA
  * certificate instead of a Gcrypt public key.  Note that this
  * function may return NULL on error.  */
