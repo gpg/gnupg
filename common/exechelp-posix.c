af4a6fff0da512defb390e9473d95edd77956dff
NIIBE Yutaka <gniibe@fsij.org>
2022-11-25 11:57:57 +0900

    Fix gnupg_process_release.
    Signed-off-by: NIIBE Yutaka <gniibe@fsij.org>

--- common/exechelp-posix.c
+++ common/exechelp-posix.c
@@ -1529,8 +1529,14 @@ gnupg_process_wait (gnupg_process_t process, int hang)
 void
 gnupg_process_release (gnupg_process_t process)
 {
+  if (!process)
+    return;
+
   if (process->terminated)
-    gnupg_process_wait (process, 0);
+    {
+      gnupg_process_terminate (process);
+      gnupg_process_wait (process, 1);
+    }
 
   xfree (process);
 }
