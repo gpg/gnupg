gpg/gnupg

commit 7571fd4cd02571ddd0ba84700c0a52704f0a9b5c

Change spawn_cb semantics.
--

So that the callback can replace fds for stdin/out/err.

Signed-off-by: NIIBE Yutaka <gniibe@fsij.org>

common/exechelp-posix.c
@@ -993,30 +993,32 @@ my_exec (const char *pgmname, const char *argv[],
          int (*spawn_cb) (void *), void *spawn_cb_arg)
 {
   int i;
-  int fds[3];
   int ask_inherit = 0;
+  struct spawn_cb_arg sca;
 
-  fds[0] = fd_in;
-  fds[1] = fd_out;
-  fds[2] = fd_err;
+
+  sca.fds[0] = fd_in;
+  sca.fds[1] = fd_out;
+  sca.fds[2] = fd_err;
+  sca.arg = spawn_cb_arg;
 
   /* Assign /dev/null to unused FDs.  */
   for (i = 0; i <= 2; i++)
     if (fds[i] == -1)
-      fds[i] = posix_open_null (i);
+      sca.fds[i] = posix_open_null (i);
+
+  if (spawn_cb)
+    ask_inherit = (*spawn_cb) (&sca);
 
   /* Connect the standard files.  */
   for (i = 0; i <= 2; i++)
-    if (fds[i] != i)
+    if (sca.fds[i] != i)
       {
-        if (dup2 (fds[i], i) == -1)
+        if (dup2 (sca.fds[i], i) == -1)
           log_fatal ("dup2 std%s failed: %s\n",
                      i==0?"in":i==1?"out":"err", strerror (errno));
-        close (fds[i]);
+        close (sca.fds[i]);
       }
-
-  if (spawn_cb)
-    ask_inherit = (*spawn_cb) (spawn_cb_arg);
 
   /* Close all other files.  */
   if (!ask_inherit)

common/exechelp.h
@@ -211,6 +211,7 @@ gpg_error_t gnupg_spawn_process_detached (const char *pgmname,
 /* The opaque type for a subprocess.  */
 typedef struct gnupg_process *gnupg_process_t;
 #ifdef HAVE_W32_SYSTEM
+struct spawn_cb_arg;
 #ifdef NEED_STRUCT_SPAWN_CB_ARG
 struct spawn_cb_arg {
   void *plpAttributeList;
@@ -218,6 +219,11 @@ struct spawn_cb_arg {
   void *arg;
 };
 #endif
+#else
+struct spawn_cb_arg {
+  int fds[3];
+  void *arg;
+};
 #endif
 
 /* Internal flag to ihnerit file descriptor/handle */
@@ -245,7 +251,7 @@ struct spawn_cb_arg {
 /* Spawn PGMNAME.  */
 gpg_err_code_t gnupg_process_spawn (const char *pgmname, const char *argv[],
                                     unsigned int flags,
-                                    int (*spawn_cb) (void *),
+                                    int (*spawn_cb) (struct spawn_cb_arg *),
                                     void *spawn_cb_arg,
                                     gnupg_process_t *r_process);
 

commit 60b6cfe71f15e68c8c21daf5da9f4609318e2500

posix: Fix for spawn_cb.
--

Fixes: 7571fd4cd02571ddd0ba84700c0a52704f0a9b5c
Signed-off-by: NIIBE Yutaka <gniibe@fsij.org>

common/exechelp-posix.c
@@ -1000,11 +1000,12 @@ my_exec (const char *pgmname, const char *argv[],
   sca.fds[0] = fd_in;
   sca.fds[1] = fd_out;
   sca.fds[2] = fd_err;
+  sca.except_fds = NULL;
   sca.arg = spawn_cb_arg;
 
   /* Assign /dev/null to unused FDs.  */
   for (i = 0; i <= 2; i++)
-    if (fds[i] == -1)
+    if (sca.fds[i] == -1)
       sca.fds[i] = posix_open_null (i);
 
   if (spawn_cb)
@@ -1022,7 +1023,7 @@ my_exec (const char *pgmname, const char *argv[],
 
   /* Close all other files.  */
   if (!ask_inherit)
-    close_all_fds (3, NULL);
+    close_all_fds (3, sca.except_fds);
 
   execv (pgmname, (char *const *)argv);
   /* No way to print anything, as we have may have closed all streams. */
@@ -1106,7 +1107,8 @@ spawn_detached (gnupg_process_t process,
 gpg_err_code_t
 gnupg_process_spawn (const char *pgmname, const char *argv1[],
                      unsigned int flags,
-                     int (*spawn_cb) (void *), void *spawn_cb_arg,
+                     int (*spawn_cb) (struct spawn_cb_arg *),
+                     void *spawn_cb_arg,
                      gnupg_process_t *r_process)
 {
   gpg_err_code_t ec;

common/exechelp.h
@@ -222,6 +222,7 @@ struct spawn_cb_arg {
 #else
 struct spawn_cb_arg {
   int fds[3];
+  int *except_fds;
   void *arg;
 };
 #endif

common/exectool.c
@@ -302,12 +302,12 @@ copy_buffer_flush (struct copy_buffer *c, estream_t sink)
 
 
 static int
-close_all_except (void *arg)
+setup_close_all (struct spawn_cb_arg *sca)
 {
-  int *exceptclose = arg;
+  int *user_except = sca->arg;
 
-  close_all_fds (3, exceptclose);
-  return 1;
+  sca->except_fds = user_except;
+  return 0;
 }
 
 /* Run the program PGMNAME with the command line arguments given in
@@ -424,7 +424,7 @@ gnupg_exec_tool_stream (const char *pgmname, const char *argv[],
                                : GNUPG_PROCESS_STDIN_NULL)
                               | GNUPG_PROCESS_STDOUT_PIPE
                               | GNUPG_PROCESS_STDERR_PIPE),
-                             close_all_except, exceptclose, &proc);
+                             setup_close_all, exceptclose, &proc);
   gnupg_process_get_streams (proc, GNUPG_PROCESS_STREAM_NONBLOCK,
                              input? &infp : NULL, &outfp, &errfp);
   if (extrapipe[0] != -1)