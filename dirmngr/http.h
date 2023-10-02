7f2f970540b68df75f83cfb30c501da1008bdd92
Werner Koch <wk@gnupg.org>
2023-10-02 12:53:41 +0200

    dirmngr: Extended the http_get_header function.
    * dirmngr/http.c (send_request): Add arg 'skip'.  Adjust all callers.
    --
    
    GnuPG-bug-id: 6719

--- dirmngr/http.h
+++ dirmngr/http.h
@@ -193,7 +193,7 @@ estream_t http_get_read_ptr (http_t hd);
 estream_t http_get_write_ptr (http_t hd);
 unsigned int http_get_status_code (http_t hd);
 const char *http_get_tls_info (http_t hd, const char *what);
-const char *http_get_header (http_t hd, const char *name);
+const char *http_get_header (http_t hd, const char *name, unsigned int skip);
 const char **http_get_header_names (http_t hd);
 gpg_error_t http_verify_server_credentials (http_session_t sess);
 
