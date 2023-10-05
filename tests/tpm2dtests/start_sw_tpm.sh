227b3b14f4be2f33ed721818c2186e7fca4cebdf
NIIBE Yutaka <gniibe@fsij.org>
2023-10-05 10:21:35 +0900

    tests:tpm2dtests: Modify tests with SWTPM and relax the condition.
    * configure.ac (SWTPM_IOCTL): Remove.
    (TEST_LIBTSS): Fix the condition.
    * tests/tpm2dtests/Makefile.am (TESTS_ENVIRONMENT): Remove
    SWTPM_IOCTL.
    * tests/tpm2dtests/start_sw_tpm.sh: Add --flags to invoke SWTPM,
    not requiring SWTPM_IOCTL and TSSSTARTUP any more.
    
    --
    
    GnuPG-bug-id: 6052
    Signed-off-by: NIIBE Yutaka <gniibe@fsij.org>

--- tests/tpm2dtests/start_sw_tpm.sh
+++ tests/tpm2dtests/start_sw_tpm.sh
@@ -3,36 +3,35 @@
 # remove any prior TPM contents
 rm -f NVChip h*.bin *.permall
 if [ -x "${SWTPM}" ]; then
-    ${SWTPM} socket --tpm2 --daemon      \
-	     --pid file=swtpm.pid        \
-	     --server type=tcp,port=2321 \
-             --ctrl type=tcp,port=2322 --tpmstate dir=`pwd`
-    pid=$(cat swtpm.pid)
+    ${SWTPM} socket --tpm2 --daemon              \
+             --pid file=swtpm.pid                \
+             --server type=tcp,port=2321         \
+             --ctrl type=tcp,port=2322           \
+             --flags not-need-init,startup-clear \
+             --tpmstate dir=`pwd`
+    cat swtpm.pid
 else
     ${TPMSERVER} > /dev/null 2>&1  &
     pid=$!
-fi
-##
-# This powers on the tpm and starts it
-# then we derive the RSA version of the storage seed and
-# store it permanently at handle 81000001 and flush the transient
-##
-a=0; while [ $a -lt 10 ]; do
-    if [ -x "${SWTPM_IOCTL}" ]; then
-	${SWTPM_IOCTL} --tcp 127.0.0.1:2322 -i > /dev/null 2>&1
-    else
-	tsspowerup > /dev/null 2>&1
+    ##
+    # This powers on the tpm and starts it
+    # then we derive the RSA version of the storage seed and
+    # store it permanently at handle 81000001 and flush the transient
+    ##
+    a=0
+    while [ $a -lt 10 ]; do
+        tsspowerup > /dev/null 2>&1
+        if [ $? -eq 0 ]; then
+            break;
+        fi
+        sleep 1
+        a=$[$a+1]
+    done
+    if [ $a -eq 10 ]; then
+        echo "Waited 10s for tpm_server to come up; exiting"
+        exit 1
     fi
-    if [ $? -eq 0 ]; then
-	break;
-    fi
-    sleep 1
-    a=$[$a+1]
-done
-if [ $a -eq 10 ]; then
-    echo "Waited 10s for tpm_server to come up; exiting"
-    exit 1
-fi
 
-tssstartup || exit 1
-echo -n $pid
+    ${TSSSTARTUP} || exit 1
+    echo -n $pid
+fi
