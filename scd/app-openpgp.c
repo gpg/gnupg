984a0c6982cc7beb42300f18b53301c67c6ec49c
NIIBE Yutaka <gniibe@fsij.org>
2024-03-27 10:25:13 +0900

    scd:openpgp: Fix data_objects specification for F9 and FA.
    * scd/app-openpgp.c (data_objects): These are constructed objects.
    
    --
    
    GnuPG-bug-id: 7058
    Signed-off-by: NIIBE Yutaka <gniibe@fsij.org>

--- scd/app-openpgp.c
+++ scd/app-openpgp.c
@@ -119,8 +119,8 @@ static struct {
   { 0x00D6, 0, 0x6E, 1, 0, 0, 0, 0, "UIF for Signature"},
   { 0x00D7, 0, 0x6E, 1, 0, 0, 0, 0, "UIF for Decryption"},
   { 0x00D8, 0, 0x6E, 1, 0, 0, 0, 0, "UIF for Authentication"},
-  { 0x00F9, 0,    0, 1, 0, 0, 0, 0, "KDF data object"},
-  { 0x00FA, 0,    0, 1, 0, 0, 0, 2, "Algorithm Information"},
+  { 0x00F9, 1,    0, 1, 0, 0, 0, 0, "KDF data object"},
+  { 0x00FA, 1,    0, 1, 0, 0, 0, 2, "Algorithm Information"},
   { 0 }
 };
 
