34045ed9e1235d0d31fde218bb286e4b0d96a73e
NIIBE Yutaka <gniibe@fsij.org>
2024-05-30 13:39:31 +0900

    common: Fix process termination check at release.
    * src/exechelp-posix.c (gnupg_process_release): When NOT terminated,
    terminate and wait.
    * src/exechelp-w32.c (gnupg_process_release): Likewise.
    
    --
    
    Signed-off-by: NIIBE Yutaka <gniibe@fsij.org>

--- common/exechelp-posix.c
+++ common/exechelp-posix.c
@@ -997,7 +997,7 @@ gnupg_process_release (gnupg_process_t process)
   if (!process)
     return;
 
-  if (process->terminated)
+  if (!process->terminated)
     {
       gnupg_process_terminate (process);
       gnupg_process_wait (process, 1);
--- common/exechelp-w32.c
+++ common/exechelp-w32.c
@@ -1209,7 +1209,7 @@ gnupg_process_release (gnupg_process_t process)
   if (!process)
     return;
 
-  if (process->terminated)
+  if (!process->terminated)
     {
       gnupg_process_terminate (process);
       gnupg_process_wait (process, 1);
