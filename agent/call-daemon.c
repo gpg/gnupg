40707c8bff49f106f8b0c09fee1af08467448bfc
NIIBE Yutaka <gniibe@fsij.org>
2024-10-22 09:51:03 +0900

    agent: Fix resource leak for PRIMARY_CTX.
    * agent/call-daemon.c (wait_child_thread): Call assuan_release for
    PRIMARY_CTX when it's kept for reuse.
    
    --
    
    Signed-off-by: NIIBE Yutaka <gniibe@fsij.org>

--- agent/call-daemon.c
+++ agent/call-daemon.c
@@ -134,6 +134,9 @@ wait_child_thread (void *arg)
             }
         }
 
+      if (g->primary_ctx_reusable)
+        assuan_release (g->primary_ctx);
+
       g->primary_ctx = NULL;
       g->primary_ctx_reusable = 0;
 
