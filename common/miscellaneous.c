7b1356a58b53744a1c197989972c855bd6132548
nicesoft-labs <info@ncsgp.ru>
2025-06-16 18:02:24 +0300

    Update miscellaneous.c

--- common/miscellaneous.c
+++ common/miscellaneous.c
@@ -145,6 +145,21 @@ gnupg_cipher_algo_name (int algo)
   return s;
 }
 
+/* A wrapper around gcry_pk_algo_name to return the string
+   "ECC (incl. GOST)" instead of "ECC" if GOST is supported. */
+const char *
+gnupg_pk_algo_name (int algo)
+{
+  const char *s;
+
+  s = gcry_pk_algo_name (algo);
+  if (!strcmp (s, "ECC")
+      && openpgp_is_curve_supported ("GOST2012-256-A", NULL, NULL))
+    s = "ECC (incl. GOST)";
+  return s;
+}
+
+
 
 void
 obsolete_option (const char *configname, unsigned int configlineno,
@@ -608,6 +623,38 @@ parse_debug_flag (const char *string, unsigned int *debugvar,
   return 0;
 }
 
+
+void
+flip_buffer (unsigned char *buffer, unsigned int length)
+{
+  unsigned int tmp, i;
+
+  for (i=0; i < length/2; i++) {
+      tmp = buffer[i];
+      buffer[i] = buffer[length-1-i];
+      buffer[length-1-i] = tmp;
+  }
+}
+
+int
+mpi_byte_flip (gcry_mpi_t val, gcry_mpi_t *flipped)
+{
+	int rc;
+	unsigned char *buffer = NULL;
+	size_t len = 0;
+	size_t slen = 0;
+
+	rc = gcry_mpi_aprint (GCRYMPI_FMT_USG, &buffer, &len, val);
+	if (0 == rc && buffer) {
+		flip_buffer (buffer, len);
+		rc = gcry_mpi_scan (flipped, GCRYMPI_FMT_USG, buffer, len, &slen);
+		if (0 == rc && slen != len) rc = 1;
+	}
+
+	if (buffer) gcry_free (buffer);
+
+	return rc;
+}
 
 
 /* Parse an --comaptibility_flags style argument consisting of comma
